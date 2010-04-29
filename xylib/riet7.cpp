4685cbb625a6356baa80f7744c8b53ec43c9c367
Marcin Wojdyr <wojdyr@gmail.com>
2010-04-29 00:05:14 +0000

    changed function check() in riet7 format

--- xylib/riet7.cpp
+++ xylib/riet7.cpp
@@ -22,15 +22,54 @@ const FormatInfo Riet7DataSet::fmt_info(
     &Riet7DataSet::check
 );
 
+static
+int count_numbers(const char* p)
+{
+    int n;
+    while (*p != 0) {
+        char *endptr;
+        strtod(p, &endptr);
+        if (p == endptr) // no more numbers
+            break;
+        ++n;
+        p = endptr;
+    }
+    return n;
+}
 
+// .dat is popular extension for data, we want to avoid false positives.
+// This format has line starting with three numbers: start step end
+// somewhere in the first few lines, and it has intensities (in any format)
+// in the following lines. We use here following rules:
+// - try to read start-step-end from the first 6 lines:
+//    one of the first 6 lines must start with three numbers, such that
+//    n = (end - start) / step is an integer,
+// - the start-step-end line and the next line must have different format
+//   (if they have the same format, it's more likely xy format)
 bool Riet7DataSet::check(istream &f)
 {
+    char line[256];
     for (int i = 0; i < 6; ++i) {
-        Column *c = read_start_step_end_line(f);
-        if (c) {
-            delete c;
-            return true;
-        }
+        f.getline(line, 255);
+        int n = count_numbers(line);
+        if (n < 3)
+            continue;
+
+        const char *startptr = line;
+        char *endptr;
+        double start = strtod(startptr, &endptr);
+        startptr = endptr;
+        double step = strtod(startptr, &endptr);
+        startptr = endptr;
+        double stop = strtod(endptr, &endptr);
+        double dcount = (stop - start) / step + 1;
+        int count = iround(dcount);
+        if (count < 4 || fabs(count - dcount) > 1e-2)
+            continue;
+
+        f.getline(line, 255);
+        int n2 = count_numbers(line);
+        return n2 != n;
     }
     return false;
 }
