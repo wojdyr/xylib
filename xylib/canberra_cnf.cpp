00b8696a437d3dcbe67776cb0e635cacc44cd622
Marcin Wojdyr <wojdyr@gmail.com>
2011-12-22 23:43:52 +0000

    completely rewritten reading of CNF format

--- xylib/canberra_cnf.cpp
+++ xylib/canberra_cnf.cpp
@@ -1,11 +1,13 @@
-// Canberra CNF (one of variants)
+// Canberra CNF (aka CAM format) with spectral data from Genie software
 // Licence: Lesser GNU Public License 2.1 (LGPL)
-// $Id: $
+// $Id$
 
 #define BUILDING_XYLIB
 #include "canberra_cnf.h"
 
-#include <cmath>
+#include <memory>
+#include <ctime>
+#include <cstring>
 #include <boost/cstdint.hpp>
 
 #include "util.h"
@@ -14,6 +16,7 @@ using namespace std;
 using namespace xylib::util;
 using boost::uint16_t;
 using boost::uint32_t;
+using boost::uint64_t;
 
 
 namespace xylib {
@@ -31,67 +34,198 @@ const FormatInfo CanberraCnfDataSet::fmt_info(
 
 bool CanberraCnfDataSet::check(istream &f)
 {
-    const int size = 0x8c0;
-    char *data = new char[size];
-    f.read(data, size);
-    // some cnf files have "PHA+" at 0x8b0 and number of channels at 0x8b9
-    bool has_pha = (data[0x8b0] == 'P' && data[0x8b1] == 'H' &&
-                    data[0x8b2] == 'A');
-    delete [] data;
-    return f.gcount() == size && has_pha;
+    int chan_offset = 0;
+    f.ignore(112);
+    int pos = 112;
+    char buf[48];
+    while (!f.eof()) {
+        f.read(buf, 48);
+        if (f.gcount() != 48)
+            return false;
+        pos += 48;
+        if (buf[0] == 5 && ((buf[1] == 0x20 && buf[2] == 0x01) ||
+                             buf[1] == 0 || buf[2] == 0)) {
+            chan_offset = from_le<uint32_t>(buf+10);
+            break;
+        }
+    }
+    if (chan_offset <= pos)
+        return false;
+    f.ignore(chan_offset - pos);
+    f.read(buf, 48);
+    return (!f.eof() && f.gcount() == 48 && buf[0] == 5 && buf[1] == 0x20);
 }
+
+static
+Column* read_energy_callibration(const char* p, Block *blk)
+{
+    // energy calibration
+    p += 2*4+28;
+    double coef[3];
+    for (int i = 0; i != 3; ++i)
+        coef[i] = from_pdp11(p + 4*i);
+    if (coef[1] == 0.)
+        return NULL;
+    for (int i = 0; i != 3; ++i)
+        blk->meta["energy calib " + S(i)] = S(coef[i]);
+    //coef[0] += coef[1] + coef[2]; // set value from CH1 to CH0
+    if (coef[2] != 0.) // quadr term
+        return new QuadraticColumn(coef[0], coef[1], coef[2]);
+    else
+        return new StepColumn(coef[0], coef[1]);
+}
+
+static
+bool is_printable(const string &s)
+{
+    for (size_t i = 0; i != s.size(); ++i)
+        if (!isprint(s[i]))
+                return false;
+    return true;
+}
+
+// comment from Stefan Schneider-Kennedy:
+// > 8 byte little endian. Number of 100ns intervals since 1859.
+// > Like Microsoft's FILETIME with an offset.
+static
+string convert_date(const char* p)
+{
+    uint64_t d;
+    memcpy(&d, p, sizeof(d));
+    le_to_host(&d, sizeof(d));
+    time_t t = d / 1e7 - 3506716800; // time since the Epoch
+    char s[64];
+    int r = strftime(s, sizeof(s), "%a, %Y-%m-%d %H:%M:%S", gmtime(&t));
+    if (r == 0)
+        throw FormatError("reading date failed.");
+    return string(s);
+}
+
+// comment from Stefan Schneider-Kennedy:
+// > 8 byte little endian. 2^(64) - number of 100ns intervals since timing
+// > started. Like Microsoft's FILETIME. Round to nearest second.
+static
+float convert_time(const char* p)
+{
+    uint64_t d;
+    memcpy(&d, p, sizeof(d));
+    le_to_host(&d, sizeof(d));
+    return (~d) * 1.0e-7;
+}
+
 
 void CanberraCnfDataSet::load_data(std::istream &f)
 {
-    const int max_size = 0x17DFC;
-    char *data = new char[max_size];
-    f.read(data, max_size);
-    int size = f.gcount();
-    if (size < 0x5870) {
-        delete [] data;
-        throw FormatError("Read error or unexpected end of file.");
-    }
+    string file_string;
+    file_string.reserve(128*1024);
+    file_string.assign((istreambuf_iterator<char>(f)),
+                       istreambuf_iterator<char>());
+    const char* beg = file_string.c_str();
+    const char* end = beg + file_string.size();
 
-    // energy calibration is there, but where exactly?
-    double energy_offset = 0;
-    double energy_slope = 1.;
-    double energy_quadr = 0.;
+    int acq_offset = 0, sam_offset = 0, eff_offset = 0, enc_offset = 0,
+        chan_offset = 0;
 
-    uint16_t n_channels = *reinterpret_cast<uint16_t*>(data+0x8b9);
-    le_to_host(&n_channels, 2);
-    int data_offset = size - 4 * n_channels;
-    if (data_offset < 0x5870) {
-        delete [] data;
-        throw FormatError("apparently we do not know how to read this file.");
-    }
-
-    Block* blk = new Block;
-
-    Column *xcol = NULL;
-    if (energy_quadr) {
-        VecColumn *vc = new VecColumn;
-        for (int i = 1; i <= n_channels; i++) {
-            double x = energy_offset + energy_slope * i + energy_quadr * i * i;
-            vc->add_val(x);
+    // we have here 48-byte blocks with meta data
+    for (const char* ptr = beg+112; ptr+48 < end; ptr += 48) {
+        uint32_t offset = from_le<uint32_t>(ptr+10);
+        if ((ptr[1] == 0x20 && ptr[2] == 0x01) || ptr[1] == 0 || ptr[2] == 0) {
+            switch (ptr[0]) {
+                case 0:
+                    if (acq_offset == 0)
+                        acq_offset = offset;
+                    else
+                        enc_offset = offset;
+                    break;
+                case 1:
+                    if (sam_offset == 0)
+                        sam_offset = offset;
+                    break;
+                case 2:
+                    if (eff_offset == 0)
+                        eff_offset = offset;
+                    break;
+                case 5:
+                    if (chan_offset == 0)
+                        chan_offset = offset;
+                    break;
+                default:
+                    break;
+            }
+            if (acq_offset != 0 && sam_offset != 0 &&
+                        eff_offset != 0 && chan_offset != 0)
+                break;
         }
-        xcol = vc;
     }
-    else {
-        xcol = new StepColumn(energy_offset+energy_slope, energy_slope);
-    }
-    blk->add_column(xcol);
+    if (enc_offset == 0)
+        enc_offset = acq_offset;
 
+    auto_ptr<Block> blk(new Block);
+
+    // sample data - split name into name and description
+    // (this was not in code from JF, it's my guess - MW)
+    const char* sam_ptr = beg + sam_offset;
+    string name;
+    if (sam_ptr+0x30+80 >= end || sam_ptr[0] != 1 || sam_ptr[1] != 0x20)
+        fprintf(stderr, "Warning. Sample data not found.\n");
+    else {
+        string name = str_trim(string(sam_ptr+0x30, 32));
+        if (!name.empty() && is_printable(name))
+            blk->set_name(name);
+        string desc = str_trim(string(sam_ptr+0x30+32, 48));
+        if (!desc.empty() && is_printable(desc))
+            blk->meta["description"] = desc;
+    }
+
+    // go to acquisition data
+    const char* acq_ptr = beg + acq_offset;
+    if (acq_ptr+48+128+10+4 >= end || acq_ptr[0] != 0 || acq_ptr[1] != 0x20)
+        throw FormatError("Acquisition data not found.");
+    uint16_t offset1 = from_le<uint16_t>(acq_ptr+34);
+    uint16_t offset2 = from_le<uint16_t>(acq_ptr+36);
+
+    const char* pha_ptr = acq_ptr + 48 + 128;
+    if (pha_ptr[0] != 'P' || pha_ptr[1] != 'H' || pha_ptr[2] != 'A')
+        fprintf(stderr, "Warning. PHA keyword not found.\n");
+
+    int n_channels = from_le<uint16_t>(pha_ptr+10) * 256;
+    if (n_channels < 256 || n_channels > 16384)
+        throw FormatError("Unexpected number of channels" + S(n_channels));
+
+    // dates and times
+    const char* date_ptr = acq_ptr+48+offset2+1;
+    format_assert(this, date_ptr + 3*8 < end);
+    blk->meta["date and time"] = convert_date(date_ptr); // when taken
+    float real_time = convert_time(date_ptr + 8);
+    blk->meta["real time (s)"] = format1<float, 16>("%.2f", real_time);
+    float live_time = convert_time(date_ptr + 16);
+    blk->meta["live time (s)"] = format1<float, 16>("%.2f", live_time);
+
+    format_assert(this, beg + enc_offset+48+32 + offset1 < end);
+    Column *xcol = read_energy_callibration(beg + enc_offset+48+32 + offset1,
+                                            blk.get());
+    if (xcol == NULL)
+        xcol = read_energy_callibration(beg + enc_offset+48+32, blk.get());
+    if (xcol == NULL)
+        fprintf(stderr, "Warning. Energy Calibration not found.\n");
+
+    // code from JF is also reading detector name, but it's not needed here
+    // detector name was 232 bytes after energy calibration
+
+    // channel data
+    const char* chan_ptr = beg + chan_offset;
+    if (chan_ptr+512+4*n_channels > end || chan_ptr[0] != 5 ||
+                                           chan_ptr[1] != 0x20)
+        throw FormatError("Channel data not found.");
     VecColumn *ycol = new VecColumn;
-    for (int i = 0; i < n_channels; i++) {
-        uint32_t y = *reinterpret_cast<uint32_t*>(data+data_offset+4*i);
-        le_to_host(&y, 4);
+    for (int i = 0; i < n_channels; ++i) {
+        uint32_t y = from_le<uint32_t>(chan_ptr+512+4*i);
         ycol->add_val(y);
     }
-    blk->add_column(ycol);
 
-    //blk->set_name(str_trim(string(data+0x5830, 63)));
-    blk->set_name(str_trim(string(data+0x5870, 16)));
-    add_block(blk);
+    blk->add_column(xcol);
+    blk->add_column(ycol);
+    add_block(blk.release());
 }
 
 } // namespace xylib
--- xylib/canberra_cnf.h
+++ xylib/canberra_cnf.h
@@ -1,11 +1,17 @@
-// Canberra CNF (aka CAM format), one of many variants
+// Canberra CNF (aka CAM format) with spectral data from Genie software
 // Licence: Lesser GNU Public License 2.1 (LGPL)
-// $Id: $
+// $Id$
 
-// based on:
-//  - this converting script:
+// Based on code from Jim Fitzgerald that is used in FitzPeaks (Gamma Analysis
+// and Calibration Software, http://www.jimfitz.demon.co.uk/fitzpeak.htm).
+//
+// I have rewritten large part of this code and may have introduced bugs.
+// Let me know if you find one - Marcin W.
+//
+// The first attempt to add this format was based on script from:
 //    http://www.physicsforums.com/showpost.php?p=2279239&postcount=11
-//  - analysis of a sample from Canberra DSA1000 MCA w/ Genie 2000 software.
+// and on analysis of a sample from Canberra DSA1000 MCA w/ Genie 2000 software,
+// but it was almost completely rewritten when JF sent me his code.
 
 
 #ifndef XYLIB_CANBERRA_CNF_H_
