cf01efda17a337cebde0fda5d4603f302338ecb8
Marcin Wojdyr <wojdyr@gmail.com>
2013-08-01 03:50:05 +0100

    added specsxy.h/cpp files from Matthias Richter

---
+++ xylib/specsxy.h
@@ -0,0 +1,18 @@
+// Specslabs xy data file
+// Licence: Lesser GNU Public License 2.1 (LGPL)
+
+
+#ifndef XYLIB_SPECSXY_H_
+#define XYLIB_SPECSXY_H_
+#include "xylib.h"
+
+namespace xylib {
+
+    class SpecsxyDataSet : public DataSet
+    {
+        OBLIGATORY_DATASET_MEMBERS(SpecsxyDataSet)
+    };
+
+}
+#endif // XYLIB_SPECSXY_H_
+
