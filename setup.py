dace86c546fbccf5e08c751aedf3ef1089b9432d
Marcin Wojdyr <wojdyr@gmail.com>
2016-12-17 23:16:52 +0000

    setup.py: fill more fields for PyPI

--- setup.py
+++ setup.py
@@ -2,6 +2,16 @@
 
 # This setup.py builds xylib as a python extension, which is experimental.
 # The normal way of building xylib is using configure && make. Or cmake.
+long_description="""\
+xylib is a library for reading obscure file formats with data from
+powder diffraction, spectroscopy and other experimental methods.
+For the list of supported formats see https://github.com/wojdyr/xylib .
+
+This module includes bindings to xylib and xylib itself.
+The first two numbers in the version are the version of included xylib.
+
+Prerequisites for building: SWIG and Boost libraries.
+"""
 
 from distutils.core import setup, Extension
 from distutils.command.sdist import sdist
@@ -39,8 +49,17 @@ if sys.version_info[0] == 3:
     swig_opts += ['-py3']
 
 setup(name='xylib-py',
-      version='0.1.1',
+      version='1.5.0',
       description='Python bindings to xylib',
+      long_description=long_description,
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Topic :: Scientific/Engineering :: Chemistry',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          ],
       author='Marcin Wojdyr',
       author_email='wojdyr@gmail.com',
       license='LGPL2.1',
