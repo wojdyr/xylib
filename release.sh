284f34fcbfd16faef806cb5b9db1c14b799ba80e
Marcin Wojdyr <wojdyr@gmail.com>
2016-12-17 23:16:06 +0000

    update release.sh

--- release.sh
+++ release.sh
@@ -11,10 +11,11 @@ if [ $# -eq 0 ]; then
     echo 3. make Windows binaries
     echo 4. RPMs: https://build.opensuse.org/project/show?project=home%3Awojdyr
 #    echo 5. prepare DEBs in Launchpad  /obsolete/
+    echo p. update xylib-py in PyPI
     echo "6. git tag -a v$VERSION -m 'version $VERSION'; git push --tags"
     echo "   (and upload tarball and binaries)"
     echo 7. update webpage
-    echo 8. announce
+    echo 8. draft github release / announce
     echo 9. make Windows static lib for linking with fityk
     exit
 fi
@@ -22,12 +23,14 @@ fi
 arg=$1
 
 # 0. check version numbers and soname
-if [ $arg -eq 0 ]; then
+if [ $arg = 0 ]; then
     echo version: $VERSION
     echo configure.ac:
     grep AC_INIT configure.ac
     echo in xylib/xylib.h:
     grep 'define XYLIB_VERSION' xylib/xylib.h
+    echo in setup.py:
+    grep "version=" setup.py
     echo in README.rst:
     grep '.tar.bz2' README.rst
     grep '.zip' README.rst
@@ -49,7 +52,7 @@ if [ $arg = c ]; then
 fi
 
 # 1. test compilation from git
-if [ $arg -eq 1 ]; then
+if [ $arg = 1 ]; then
     rm -rf git_copy
     mkdir git_copy
     cd git_copy
@@ -64,7 +67,7 @@ if [ $arg -eq 1 ]; then
 fi
 
 # 2. make tarball
-if [ $arg -eq 2 ]; then
+if [ $arg = 2 ]; then
     gunzip git_copy/xylib/xylib-*.tar.gz
     bzip2 git_copy/xylib/xylib-*.tar
     mv git_copy/xylib/xylib-*.tar.bz2 . && echo OK
@@ -72,7 +75,7 @@ if [ $arg -eq 2 ]; then
 fi
 
 # 3. make Windows binaries 
-if [ $arg -eq 3 ]; then
+if [ $arg = 3 ]; then
     mkdir -p xylib_win-$VERSION/docs
     cp -r git_copy/xylib/install_dir/include/xylib/ xylib_win-$VERSION
     rm index.html
@@ -84,7 +87,7 @@ if [ $arg -eq 3 ]; then
 fi
 
 # 5. prepare DEBs in Launchpad  /obsolete/
-if [ $arg -eq 5 ]; then
+if [ $arg = 5 ]; then
     echo go to directory with old debs, or: apt-get source xylib
     echo cd xylib-oldversion
     echo uupdate `pwd`/xylib-$VERSION.tar.bz2
@@ -97,25 +100,38 @@ if [ $arg -eq 5 ]; then
     echo etc.
 fi
 
+# p. update xylib-py in PyPI
+if [ $arg = p ]; then
+    echo "test build:"
+    echo " python setup.py build"
+    echo ' PYTHONPATH=$PWD/build/lib.*-2.7/ ./xyconv.py samples/BT86.raw tmp.xy'
+    echo "upload sdist:"
+    echo " python setup.py sdist"
+    echo " twine upload dist/xylib-py-$VERSION.0.tar.gz"
+fi
+
 # 6. GitHub file release
-if [ $arg -eq 6 ]; then
+if [ $arg = 6 ]; then
     echo "do: git tag -a v$vERSION -m 'version $VERSION'; git push --tags"
 fi
 
 # 7. update webpage
-if [ $arg -eq 7 ]; then
+if [ $arg = 7 ]; then
     make index.html
     scp index.html `whoami`,xylib@web.sourceforge.net:htdocs/
 fi
 
 # 8. announce
-if [ $arg -eq 8 ]; then
-    grep -5 "\* $VERSION" README.rst | tail -6
+if [ $arg = 8 ]; then
+    echo "-> https://github.com/wojdyr/xylib/releases/new"
+    echo "Release title: $VERSION"
+    grep -6 "\* $VERSION" README.rst | tail -7 | sed "s/^ *//"
+    echo
     echo maybe post to http://fityk-announce.nieto.pl/
 fi
 
 # 9. make Windows static lib for linking with fityk
-if [ $arg -eq 9 ]; then
+if [ $arg = 9 ]; then
     rm -rf mingw32-build
     mkdir mingw32-build
     cd mingw32-build
