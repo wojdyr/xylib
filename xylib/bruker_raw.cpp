e4403142839989718ea50b84bf0df4b15c9cce09
Tyrel M. McQueen <tmcqueen@github.com>
2022-02-03 20:44:08 -0500

    Add initial support for Bruker RAW4.00 files. Currently only supports Locked Coupled scans, expanded support in next commit.

--- xylib/bruker_raw.cpp
+++ xylib/bruker_raw.cpp
@@ -40,6 +40,11 @@ bool BrukerRawDataSet::check(istream &f, string* details)
             *details = "ver. 3";
         return true;
     }
+    else if (head == "RAW4" && read_string(f, 3) == ".00") {
+        if (details)
+            *details = "ver. 4";
+        return true;
+    }
     else
         return false;
 }
@@ -48,13 +53,15 @@ bool BrukerRawDataSet::check(istream &f, string* details)
 void BrukerRawDataSet::load_data(std::istream &f, const char*)
 {
     string head = read_string(f, 4);
-    format_assert(this, head == "RAW " || head == "RAW2" || head == "RAW1");
+    format_assert(this, head == "RAW " || head == "RAW2" || head == "RAW1" || head == "RAW4");
     if (head[3] == ' ')
         load_version1(f);
     else if (head[3] == '2')
         load_version2(f);
-    else // head[3] == '1'
+    else if (head[3] == '1')
         load_version1_01(f);
+    else // head[3] == '4'
+        load_version4(f);
 }
 
 void BrukerRawDataSet::load_version1(std::istream &f)
@@ -176,7 +183,7 @@ void BrukerRawDataSet::load_version1_01(std::istream &f)
     // file header - 712 bytes
     // the offset is already 4
     f.ignore(4); // ignore bytes 4-7
-    int file_status = read_uint32_le(f);        // address 8 
+    int file_status = read_uint32_le(f);        // address 8
     if (file_status == 1)
         meta["file status"] = "done";
     else if (file_status == 2)
@@ -304,6 +311,127 @@ void BrukerRawDataSet::load_version1_01(std::istream &f)
         add_block(blk);
     }
 }
+
+void BrukerRawDataSet::load_version4(std::istream &f)
+{
+    meta["format version"] = "4";
+
+    // format v4 consists of a 61 byte header (7+54 bytes)
+    // followed by a set of metadata segments, followed by
+    // one or more ranges corresponding to individual
+    // measurement traces.
+    // the offset is already 4
+    f.ignore(8); // ignore bytes 4-11
+    meta["MEASURE_DATE"] = read_string(f, 12);  // address 12
+    meta["MEASURE_TIME"] = read_string(f, 10);  // address 24
+    f.ignore(27);                               // address 34
+
+    // begin global metadata segment reading
+    // here we pretend to undestand only type 10 and 30
+    int segment_type = -1;
+    int segment_len = 0;
+    string tag_name;
+    while (1 == 1) {
+        segment_type = read_uint32_le(f);       // offset +0
+        if (segment_type == 0 || segment_type == 160)
+            break; // start of ranges
+        segment_len = read_uint32_le(f);        // offset +4
+        if (segment_type == 10) {
+            f.ignore(4);                        // offset +8
+            tag_name = read_string(f, 24);      // offset +12
+            meta[tag_name] = read_string(f, segment_len-36); // offset +36
+        }
+        else if (segment_type == 30) {
+            f.ignore(64);                               // offset +8
+            meta["ALPHA_AVERAGE"] = S(read_dbl_le(f));  // offset +72
+            meta["ALPHA1"] = S(read_dbl_le(f));         // offset +80
+            meta["ALPHA2"] = S(read_dbl_le(f));         // offset +88
+            meta["BETA"] = S(read_dbl_le(f));           // offset +96
+            meta["ALPHA_RATIO"] = S(read_dbl_le(f));    // offset +104
+            f.ignore(4);                                // offset +112
+            meta["ANODE_MATERIAL"] = read_string(f,4);  // offset +116
+            f.ignore(segment_len-120);                  // offset +120
+        }
+        else { // skip others
+            f.ignore(segment_len-8);
+        }
+    }
+
+    // now process ranges
+    while (segment_type == 0 || segment_type == 160) {
+        Block* blk = new Block;
+        f.ignore(28);                               // offset +4
+        blk->meta["SCAN_TYPE"] = read_string(f,24); // offset +32
+        // we only support Locked Coupled Scans
+        if (blk->meta["SCAN_TYPE"] == "Locked Coupled") {
+            f.ignore(16);                           // offset +56
+            double start_2theta = read_dbl_le(f);   // offset +72
+            blk->meta["START_2THETA"] = S(start_2theta);
+            double step_size = read_dbl_le(f);      // offset +80
+            blk->meta["STEP_SIZE"] = S(step_size);
+            int steps = read_uint32_le(f);          // offset +88
+            blk->meta["STEPS"] = S(steps);
+            blk->meta["TIME_PER_STEP"] = S(read_flt_le(f)); // offset +92
+            f.ignore(4);                            // offset +96
+            blk->meta["GENERATOR_VOLTAGE"] = Su(read_flt_le(f)); // +100
+            blk->meta["GENERATOR_CURRENT"] = Su(read_flt_le(f)); // +104
+            f.ignore(4);                            // offset +108
+            blk->meta["USED_LAMBDA"] = S(read_dbl_le(f)); // offset +112
+            f.ignore(20);                           // offset +120
+            int hdr_size = read_uint32_le(f);       // offset +140
+            f.ignore(16);                           // offset +144
+
+            // process ranges for the remaining block headers,
+            // ignoring types we don't understand
+            while (hdr_size > 0) {
+                segment_type = read_uint32_le(f);   // offset +0
+                segment_len = read_uint32_le(f);    // offset +4
+                if (segment_type == 50) {
+                    f.ignore(4);                    // offset +8
+                    string segment_name = read_string(f,24); // offset +12
+                    if (segment_name == "Theta") {
+                        f.ignore(20);               // offset +36
+                        blk->meta["START_THETA"] = S(read_dbl_le(f)); // +56
+                        f.ignore(segment_len-64);
+                    }
+                    else if (segment_name == "Divergence Slit") {
+                        f.ignore(20);               // offset +36
+                        blk->meta["DIVERGENCE_SLIT"] = S(read_dbl_le(f)); // +56
+                        f.ignore(segment_len-64);
+                    }
+                    else { // ignore others
+                        f.ignore(segment_len-36);
+                    }
+                }
+                else {
+                    f.ignore(segment_len-8);
+                }
+                hdr_size -= segment_len;
+            }
+
+            // Now compute the x values and read the y values
+            StepColumn *xcol = new StepColumn(start_2theta, step_size);
+            blk->add_column(xcol);
+
+            VecColumn *ycol = new VecColumn;
+            for (int i = 0; i < steps; ++i) {
+                float y = read_flt_le(f);
+                ycol->add_val(y);
+            }
+            blk->add_column(ycol);
+
+            add_block(blk);
+        }
+
+        // End or next range
+        (void)f.peek(); // force eof if at end
+        if (f.eof())
+            segment_type = -1;
+        else
+            segment_type = read_uint32_le(f);
+    }
+}
+
 
 
 } // end of namespace xylib
