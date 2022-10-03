86b53fdb2a91eb5beded0364c24345a8fb5c22f1
lapressle <112499468+lapressle@users.noreply.github.com>
2022-10-03 16:39:42 -0400

    Update xrdml.cpp
    Our xrdml file format has 
    ```
    <counts unit="counts">
    ```
    instead of intensities, so I added an option to look for counts first to avoid this error. Will happily accept another solution!

--- xylib/xrdml.cpp
+++ xylib/xrdml.cpp
@@ -65,12 +65,17 @@ static Block* make_block_from_points(const ptree& data_points)
     if (blk->get_column_count() == 0)
         throw FormatError("cannot deduce x values");
 
-    string inten_str = data_points.get<string>("intensities");
+    string inten_str = data_points.get<string>("counts");
     VecColumn *ycol = new VecColumn;
     blk->add_column(ycol);
     ycol->add_values_from_str(inten_str);
     if (ycol->get_point_count() < 2)
-        throw FormatError("intensities do not look correct");
+        string inten_str = data_points.get<string>("intensities");
+    VecColumn *ycol = new VecColumn;
+    blk->add_column(ycol);
+    ycol->add_values_from_str(inten_str);
+    if (ycol->get_point_count() < 2)
+        throw FormatError("intensities/counts do not look correct");
     if (xs_col != NULL)
         xs_col->set_step(xs_col->get_step() / (ycol->get_point_count() - 1));
     //blk->set_name(title);
